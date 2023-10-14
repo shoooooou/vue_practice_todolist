cd6e6679b3d54a5fdfa4d53de2d3dd5e1a72b56c
s.nakamura <shotenba@gmail.com>
2023-10-14 11:35:09 +0900

    firstcommit

---
+++ src/components/TodoTable.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <th class="id">ID</th>
+          <th class="comment">コメント</th>
+          <th class="state">状態</th>
+          <th class="button">-</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="todo in todos" :key="item.id">
+        {{ item.id }}
+        
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    todos:[]
+  },
+
+};
+</script>
